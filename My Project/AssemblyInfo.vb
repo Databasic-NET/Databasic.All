6917f716c9cb5b660cb792c9b562df90eb6bca89
Tom Flídr <tomflidr@gmail.com>
2017-10-23 17:24:48 +0200

    VS2017

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -2,41 +2,34 @@
 Imports System.Reflection
 Imports System.Runtime.InteropServices
 
-' General Information about an assembly is controlled through the following 
+' General Information about an assembly is controlled through the following
 ' set of attributes. Change these attribute values to modify the information
 ' associated with an assembly.
 
 ' Review the values of the assembly attributes
 
 <Assembly: AssemblyTitle("Databasic.All")>
-<Assembly: AssemblyDescription("Databasic - C#/VB.NET database utility
- - focusing on queries primarily based on pure SQL commands, no linq transations
- - arranging data into primitive types, typed active record classes, collections and more
- - allowing to run any nonselect queries
-This package contains references to all database provider libraries (dll).
-You can use this package as you wish, but if you want to use 
-only a specific database type, install package like:
-- Databasic&#60;DatabaseType&#62;")>
-<Assembly: AssemblyCompany("Tom Flidr")>
-<Assembly: AssemblyProduct("Databasic.All")> 
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Databasic.All")>
 <Assembly: AssemblyCopyright("Copyright ©  2017")>
-<Assembly: AssemblyTrademark("Tom Flidr")>
+<Assembly: AssemblyTrademark("")>
 
 <Assembly: ComVisible(False)>
 
 'The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("b0bbc94e-6604-4e57-8d72-753653c80577")>
+<Assembly: Guid("c7950d2f-9138-4f9c-ab0f-f4271750c78e")>
 
 ' Version information for an assembly consists of the following four values:
 '
-'   Major Version
-'   Minor Version 
-'   Build Number
-'   Revision
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
 '
-' You can specify all the values or you can default the Build and Revision Numbers 
+' You can specify all the values or you can default the Build and Revision Numbers
 ' by using the '*' as shown below:
-' <Assembly: AssemblyVersion("1.0.*")> 
+' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("1.2.5.0")>
-<Assembly: AssemblyFileVersion("1.2.5.0")>
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
